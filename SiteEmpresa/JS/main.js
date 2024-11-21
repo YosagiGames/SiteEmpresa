59981c7052120cee644c0515b48ab046cb0fbbd3
YosagiGames <YosagiGamesCompany@gmail.com>
2024-11-21 17:15:29 -0300

    updates

--- SiteEmpresa/JS/main.js
+++ SiteEmpresa/JS/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:JS/main.js
 telaXMax = window.innerWidth;
 telaYMax = window.innerHeight; 
 style = document.getElementById("style"); 
@@ -17,4 +18,25 @@ window.onload = function() {
         
     }
     setInterval(update, 100);
+=======
+telaXMax = window.innerWidth;
+telaYMax = window.innerHeight; 
+style = document.getElementById("style"); 
+guia = document.getElementById("ferramentas");
+sessao = document.getElementsByClassName("sessao");
+
+
+
+window.onload = function() {   
+    
+    
+    function update() {
+    
+        telaX = window.innerWidth;
+        telaY = window.innerHeight;
+        console.log(telaX);
+        
+    }
+    setInterval(update, 100);
+>>>>>>> 54a41f7deb6d9d7512a613784859807e1742281d:SiteEmpresa/JS/main.js
 }
