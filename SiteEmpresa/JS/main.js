d3672326fc4c0a286653d2a3ca70da6a19c497e9
YosagiGames <YosagiGamesCompany@gmail.com>
2024-12-01 17:15:29 -0300

    remoção de func desnecessarias

--- SiteEmpresa/JS/main.js
+++ SiteEmpresa/JS/main.js
@@ -1,39 +1,18 @@
 
-telaXMax = window.innerWidth;
-telaYMax = window.innerHeight; 
-style = document.getElementById("style"); 
-guia = document.getElementById("ferramentas");
-sessao = document.getElementsByClassName("sessao");
-
 hq = document.getElementById("pag");
 pagAtual = 0;
 
 const pags = 9;
 
-window.onload = function() {   
-    
-    
-    function update() {
-    
-        telaX = window.innerWidth;
-        telaY = window.innerHeight;
-        console.log(telaX);
-        
-    }
-    setInterval(update, 100);
-}
-
 function prox(){
     if(pagAtual < pags){
         pagAtual++;
-        console.log('essa é a pag ' + pagAtual + " de " + pags );
         hq.src = './Img/comic/'+ pagAtual +'.jpg';
     }
 }
 function ant(){
     if(pagAtual > 0){
         pagAtual--;
-        console.log('essa é a pag ' + pagAtual );
         hq.src = './Img/comic/'+ pagAtual +'.jpg';
     }
 }
